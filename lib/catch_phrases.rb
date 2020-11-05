7005f65cdee8e9bc4cfdf648641990eb897a3be4
Charlie Knight <williamchknight@gmail.com>
2020-11-05 16:37:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-  
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -7,4 +6,15 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link 
+ lphrase = "It's Dangerous To Go Alone! Take This."
+ puts lphrase
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
 end
